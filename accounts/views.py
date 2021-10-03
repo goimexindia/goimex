03917375e2b7b669d6981682d00eeb1cacc8725c
shubhshaadivivah21 <shubhshaadivivah21@gmail.com>
2021-10-03 12:27:16 +0530

    new

--- accounts/views.py
+++ accounts/views.py
@@ -692,3 +692,18 @@ def expayment(request):
 @csrf_exempt
 def success(request):
     return render(request, "accounts/success.html")
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_text(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request)
+        # return redirect('home')
+        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+    else:
+        return HttpResponse('Activation link is invalid!')
